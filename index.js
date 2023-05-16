68a505dfb9bbb658f7b123a53affa9a9bcd3b4e5
Mrinal <mrinalmallik1@gmail.com>
2023-05-16 11:55:38 +0600

    product reducer

--- index.js
+++ index.js
@@ -1,44 +1,24 @@
-const { createStore } = require("redux");
-//Define const
-const INCREMENT = "INCREMENT";
-const DECREMENT = "DECREMENT";
-const RESET = "RESET";
-const INCREMENTBYVALUE = "INCREMENTBYVALUE";
-const ADDNEWUSER = "ADDNEWUSER";
+//Const define
 
-//state
-const initialCounterState = {
-  users: ["Mrinal"],
-  counter: 0,
+const GETPRODUCTS = "GETPRODUCTS";
+const ADDPRODUCTS = "ADDPRODUCTS";
+
+//initial state define
+const prductsInitialState = {
+  products: ["Pen", "Mobile"],
+  numberOfProduct: 2,
 };
 
-//Action
-
-const addNewUser = (value) => {
+//action for products
+const getProducts = () => {
   return {
-    type: ADDNEWUSER,
-    payload: value,
+    type: GETPRODUCTS,
   };
 };
 
-//Reducer
-const counterReducer = (state = initialCounterState, action) => {
-  switch (action.type) {
-    case ADDNEWUSER:
-      return {
-        users: [...state.users, action.payload],
-        counter: state.counter + 1,
-      };
-    default:
-      state;
-  }
+const addProduct = (value) => {
+  return {
+    type: ADDPRODUCTS,
+    payload: value,
+  };
 };
-
-const state = createStore(counterReducer);
-
-state.subscribe(() => {
-  console.log(state.getState());
-});
-
-state.dispatch(addNewUser("Mallik"));
-state.dispatch(addNewUser("Atrika"));
