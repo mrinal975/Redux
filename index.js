1a7fe925ab5e169590b0af73547a04f69ac5767e
Mrinal <mrinalmallik1@gmail.com>
2023-05-16 18:49:04 +0600

    redux with axios call and thunk middleware

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const { createStore } = require("redux");
+const { default: axios } = require("axios");
+const { createStore, applyMiddleware } = require("redux");
+const thunk = require("redux-thunk").default;
+
+//api url https://jsonplaceholder.typicode.com/todos
+// middleware- redux-thunk
+//axios api
+
 //constant
 const GET_TODOS_REQUEST = "GET_TODOS_REQUEST";
 const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS";
 const GET_TODOS_FAILED = "GET_TODOS_FAILED";
+const API_URL = "https://jsonplaceholder.typicode.com/todos";
 
 //initial state
 const initialTodoState = {
@@ -66,9 +74,30 @@ const todosReducer = (state = initialTodoState, action) => {
   }
 };
 
+//async action creator
+const fetchData = () => {
+  return (dispatch) => {
+    dispatch(getTodosRequest());
+    axios
+      .get(API_URL)
+      .then((res) => {
+        const title = res.data.map((todo) => {
+          return todo.title;
+        });
+        dispatch(getTodosSuccess(title));
+      })
+      .catch((error) => {
+        console.log(error.message);
+        dispatch(getTodosFailed(error.message));
+      });
+  };
+};
+
 //store
-const store = createStore(todosReducer);
+const store = createStore(todosReducer, applyMiddleware(thunk));
 
 store.subscribe(() => {
-  console.log(state.getState());
+  console.log(store.getState());
 });
+
+store.dispatch(fetchData());
