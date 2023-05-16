3f356169694ef2d1cffee49cf9bd781cea445c10
Mrinal <mrinalmallik1@gmail.com>
2023-05-16 11:36:59 +0600

    change state base on payload

--- index.js
+++ index.js
@@ -4,34 +4,19 @@ const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
 const RESET = "RESET";
 const INCREMENTBYVALUE = "INCREMENTBYVALUE";
+const ADDNEWUSER = "ADDNEWUSER";
 
 //state
 const initialCounterState = {
+  users: ["Mrinal"],
   counter: 0,
 };
 
 //Action
-const incrementCounter = () => {
-  return {
-    type: INCREMENT,
-  };
-};
 
-const decrementCounter = () => {
+const addNewUser = (value) => {
   return {
-    type: DECREMENT,
-  };
-};
-
-const resetCounter = () => {
-  return {
-    type: RESET,
-  };
-};
-
-const incrementByValue = (value) => {
-  return {
-    type: INCREMENTBYVALUE,
+    type: ADDNEWUSER,
     payload: value,
   };
 };
@@ -39,25 +24,10 @@ const incrementByValue = (value) => {
 //Reducer
 const counterReducer = (state = initialCounterState, action) => {
   switch (action.type) {
-    case INCREMENT:
+    case ADDNEWUSER:
       return {
-        ...state,
+        users: [...state.users, action.payload],
         counter: state.counter + 1,
-      };
-    case DECREMENT:
-      return {
-        ...state,
-        counter: state.counter - 1,
-      };
-    case RESET:
-      return {
-        ...state,
-        counter: 0,
-      };
-    case INCREMENTBYVALUE:
-      return {
-        ...state,
-        counter: state.counter + action.payload,
       };
     default:
       state;
@@ -70,13 +40,5 @@ state.subscribe(() => {
   console.log(state.getState());
 });
 
-state.dispatch(incrementCounter());
-state.dispatch(incrementCounter());
-state.dispatch(incrementCounter());
-
-state.dispatch(decrementCounter());
-
-state.dispatch(resetCounter());
-state.dispatch(incrementByValue(5));
-
-state.dispatch(incrementByValue(10));
+state.dispatch(addNewUser("Mallik"));
+state.dispatch(addNewUser("Atrika"));
